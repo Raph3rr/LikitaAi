9e90868912ad815c5337e0806c430209a3f0cc44
Olaseigbe Raphael <raphaeldiego79@gmail.com>
2026-03-17 19:08:22 +0000

    cors

--- HausaDoc/Backend/server.js
+++ HausaDoc/Backend/server.js
@@ -40,13 +40,7 @@ const flashModel2 = genAI2.getGenerativeModel({model: "gemini-2.0-flash-lite"});
 
 // ====== MIDDLEWARE ======
 app.use(helmet());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://likitaai.vercel.app", "http://localhost:5174", "https://localhost:5175", "https://localhost:5176"],
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 
 const limiter = rateLimit({
